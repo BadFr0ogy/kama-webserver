b40ba872943ddd78c1ed75d75b3046d60821a3fa
Ray-Loo <1374743528@qq.com>
2026-04-03 22:45:58 +0800

    Update main.cc
    修改了一处变为更优雅的输出，增加了一个简单的宏

--- src/main.cc
+++ src/main.cc
@@ -1,27 +1,32 @@
 #include <string>
 
-#include <TcpServer.h>
+#include "TcpServer.h"
 #include <Logger.h>
 #include <sys/stat.h>
 #include <sstream>
 #include "AsyncLogging.h"
 #include "LFU.h"
 #include "memoryPool.h"
+
+#define SERVER_ADDR "127.0.0.1"
+#define SERVER_PORT 8888
+
 // 日志文件滚动大小为1MB (1*1024*1024字节)
 static const off_t kRollSize = 1*1024*1024;
+
 class EchoServer
 {
 public:
-    EchoServer(EventLoop *loop, const InetAddress &addr, const std::string &name)
-        : server_(loop, addr, name)
+    EchoServer(EventLoop *loop, const InetAddress &addr, const std::string &name,TcpServer::Option option)
+        : server_(loop, addr, name,option)
         , loop_(loop)
     {
         // 注册回调函数
         server_.setConnectionCallback(
-            std::bind(&EchoServer::onConnection, this, std::placeholders::_1));
+            std::bind(&EchoServer::onConnection, this, std::placeholders::_1));//设置连接回调
         
         server_.setMessageCallback(
-            std::bind(&EchoServer::onMessage, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+            std::bind(&EchoServer::onMessage, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));//设置为可读写事件回调
 
         // 设置合适的subloop线程数量
         server_.setThreadNum(3);
@@ -56,6 +61,8 @@ private:
     EventLoop *loop_;
 
 };
+
+
 AsyncLogging* g_asyncLog = NULL;
 AsyncLogging * getAsyncLog(){
     return g_asyncLog;
@@ -68,11 +75,14 @@ AsyncLogging * getAsyncLog(){
         logging->append(msg, len);
     }
 }
+
+
 int main(int argc,char *argv[]) {
     //第一步启动日志，双缓冲异步写入磁盘.
     //创建一个文件夹
     const std::string LogDir="logs";
     mkdir(LogDir.c_str(),0755);
+
     //使用std::stringstream 构建日志文件夹
     std::ostringstream LogfilePath;
     LogfilePath << LogDir << "/" << ::basename(argv[0]); // 完整的日志文件路径
@@ -80,6 +90,7 @@ int main(int argc,char *argv[]) {
     g_asyncLog = &log;
     Logger::setOutput(asyncLog); // 为Logger设置输出回调, 重新配接输出位置
     log.start(); // 开启日志后端线程
+
     //第二步启动内存池和LFU缓存
      // 初始化内存池
     memoryPool::HashBucket::initMemoryPool();
@@ -87,15 +98,17 @@ int main(int argc,char *argv[]) {
     // 初始化缓存
     const int CAPACITY = 5;  
     KamaCache::KLfuCache<int, std::string> lfu(CAPACITY);
+
     //第三步启动底层网络模块
     EventLoop loop;
-    InetAddress addr(8080);
-    EchoServer server(&loop, addr, "EchoServer");
+    InetAddress addr(SERVER_PORT,SERVER_ADDR);
+    EchoServer server(&loop, addr, "EchoServer",TcpServer::Option::kReusePort);
     server.start();
- // 主loop开始事件循环  epoll_wait阻塞 等待就绪事件(主loop只注册了监听套接字的fd，所以只会处理新连接事件)
-    std::cout << "================================================Start Web Server================================================" << std::endl;
+    // 主loop开始事件循环  epoll_wait阻塞 等待就绪事件(主loop只注册了监听套接字的fd，所以只会处理新连接事件)
+    std::cout << std::string(48, '=') << "Start Web Server" << std::string(48, '=') << std::endl;
     loop.loop();
-    std::cout << "================================================Stop Web Server=================================================" << std::endl;
+    std::cout << std::string(48, '=') << "Stop Web Server" << std::string(49, '=') << std::endl;
+
     //结束日志打印
     log.stop();
 }
